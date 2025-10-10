bcc0bc81a8ff41579f321de08dbebd20861c63c4
DanRuizPeru <u202210764@upc.edu.pe>
2025-10-10 00:06:14 -0500

    feat(localstorage): usuario se almacena en local storage

--- src/app/iam/components/login-form/login-form.ts
+++ src/app/iam/components/login-form/login-form.ts
@@ -1,18 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule, FormGroup, FormBuilder } from '@angular/forms';
 import { User } from '../../domain/model/user.entity';
 import { UsersStore } from '../../application/users-store';
 import {TranslateModule, TranslatePipe} from '@ngx-translate/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-login-form',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, FormsModule, TranslateModule, TranslatePipe],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule, TranslateModule, TranslatePipe, RouterLink],
   templateUrl: './login-form.html',
   styleUrls: ['./login-form.css']
 })
-export class LoginForm implements OnInit {
+export class LoginForm {
   loginForm: FormGroup;
   users: User[] = [];
 
@@ -23,15 +24,19 @@ export class LoginForm implements OnInit {
     });
   }
 
-  ngOnInit(): void {
-    // Solo para probar automáticamente
-    this.login('LGraham', 'admin123');
-  }
 
   login(username: string, password: string): void {
     const user = this.ust.findUserByCredentials(username, password);
     if (user) {
       console.log('✅ Usuario encontrado:', user);
+      //guardar el usuario en local storage
+      localStorage.setItem('user', JSON.stringify(user));
+
+      // 🔵 Recuperar el usuario guardado y mostrarlo como prueba
+      const storedUser = localStorage.getItem('user');
+      if (storedUser) {
+        console.log('📦 Usuario guardado en localStorage:', JSON.parse(storedUser));
+      }
     } else {
       console.log('❌ Usuario o contraseña incorrectos');
     }
--- src/app/iam/components/signin-form/signin-form.ts
+++ src/app/iam/components/signin-form/signin-form.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule, FormGroup, FormBuilder } from '@angular/forms';
+import { User } from '../../domain/model/user.entity';
+import { UsersStore } from '../../application/users-store';
+import {TranslateModule, TranslatePipe} from '@ngx-translate/core';
+import {Router, RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-signin-form',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule, TranslateModule, TranslatePipe, RouterLink],
+  standalone: true,
   templateUrl: './signin-form.html',
   styleUrl: './signin-form.css'
 })
 export class SigninForm {
+  signinForm: FormGroup;
+  User = User;
+  constructor(private fb: FormBuilder, private ust: UsersStore) {
+    this.signinForm = this.fb.group({
+      name: [''],
+      username: [''],
+      password: [''],
 
+    })
+  }
 }
