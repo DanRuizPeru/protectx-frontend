a77972d574a9431ffbd3540dcf00402000667ddd
DanRuizPeru <u202210764@upc.edu.pe>
2025-12-04 06:21:15 -0500

    deploy tf

--- src/app/alerts/presentation/views/alerts-dashboard/alerts-dashboard.component.ts
+++ src/app/alerts/presentation/views/alerts-dashboard/alerts-dashboard.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { AlertsStore } from '../../../application/alerts-store';
 import { Alert } from '../../../domain/model/alert.entity';
 import { TranslateModule } from '@ngx-translate/core';
+import {AlertFormComponent} from '../alert-form/alert-form.component';
 
 @Component({
   selector: 'app-alerts-dashboard',
   standalone: true,
-  imports: [CommonModule, RouterLink, TranslateModule],
+  imports: [CommonModule, RouterLink, TranslateModule, AlertFormComponent],
   templateUrl: './alerts-dashboard.component.html',
   styleUrls: ['./alerts-dashboard.component.css']
 })
@@ -17,10 +18,25 @@ export class AlertsDashboardComponent {
   loading;
   error;
 
+  showCreateForm = signal(false);
+
   constructor(private alertsStore: AlertsStore) {
     this.alerts = this.alertsStore.alerts;
     this.loading = this.alertsStore.loading;
     this.error = this.alertsStore.error;
+  }
+
+  openCreateForm() {
+    this.showCreateForm.set(true);
+  }
+
+  closeCreateForm() {
+    this.showCreateForm.set(false);
+  }
+
+  saveNewAlert(alertData: Omit<Alert, 'id'>) {
+    this.alertsStore.addAlert(alertData);
+    this.showCreateForm.set(false);
   }
 
   getSeverityIcon(alert: Alert): string {
